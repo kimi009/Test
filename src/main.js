5b0c2842518d99035a6104bafb4062ea35af0c8d
chenfuye <444812002@qq.com>
2018-03-06 22:28:09 +0800

    添加代码

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import api from './api/index.js'
+Vue.prototype.$api = api
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
